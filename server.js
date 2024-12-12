3c40f87d344031bcf03eeac27bee72f3bafc0f2c
pursa <pung2319@hotmail.com>
2024-12-12 21:56:33 +0700

    Update port server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const PORT = process.env.PORT || 3001;
 
 const userController = require('./controllers/UserController');
 const productController = require('./controllers/ProductController');
@@ -16,6 +17,6 @@ app.use('/user', userController);
 app.use('/product', productController);
 app.use('/api/sale', saleController);
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001')
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
